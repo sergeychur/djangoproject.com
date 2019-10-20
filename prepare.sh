f6939c244fd07d94796287f823223007221523b2
sergeychur <churicovser@mail.ru>
2019-10-20 19:46:46 +0300

    fixed prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,6 @@
-git clone https://github.com/sergeychur/djangoproject.com.git && \
-cd ./djangoproject.com && \
 sudo apt-get update && \
 sudo apt-get remove docker docker-engine docker.io && \
-sudo apt install docker.io && \
+sudo apt install -y docker.io && \
 sudo systemctl start docker && \
 sudo systemctl enable docker && \
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
